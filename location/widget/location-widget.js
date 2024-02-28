76e8e7e2c72d0e1a47b849f2991381803e8194b6
Twinpx <info@twinpx.ru>
2024-02-28 11:43:15 +0300

    Changes

--- location/widget/location-widget.js
+++ location/widget/location-widget.js
@@ -1,39 +1,39 @@
 window.twpxYadeliveryLocation = {
-  createWidget(options, callback) {
-    fetchLocation();
+    createWidget(options, callback) {
+        fetchLocation();
 
-    async function fetchLocation() {
-      const root = document.getElementById(options.containerId);
+        async function fetchLocation() {
+            const root = document.getElementById(options.containerId);
 
-      //html
-      let responseHTML = await fetch(options.fetchLocationURL.html);
-      let html = await responseHTML.text();
+            //html
+            let responseHTML = await fetch(options.fetchLocationURL.html);
+            let html = await responseHTML.text();
 
-      root.innerHTML = html;
+            root.innerHTML = html;
 
-      //stye
-      let responseCSS = await fetch(options.fetchLocationURL.css);
-      let css = await responseCSS.text();
+            //stye
+            let responseCSS = await fetch(options.fetchLocationURL.css);
+            let css = await responseCSS.text();
 
-      const style = document.createElement('style');
-      style.textContent = css;
+            const style = document.createElement("style");
+            style.textContent = css;
 
-      document.querySelector('head').append(style);
+            document.querySelector("head").append(style);
 
-      //script
-      let responseJS = await fetch(options.fetchLocationURL.js);
-      let js = await responseJS.text();
+            //script
+            let responseJS = await fetch(options.fetchLocationURL.js);
+            let js = await responseJS.text();
 
-      const script = document.createElement('script');
-      script.textContent = js;
+            const script = document.createElement("script");
+            script.textContent = js;
 
-      document.querySelector('body').append(script);
+            document.querySelector("body").append(script);
 
-      window.twpxYadeliveryLocation.locationJS(options.params);
+            window.twpxYadeliveryLocation.locationJS(options.params);
 
-      callback();
-    }
-  },
+            callback();
+        }
+    },
 };
 
-document.dispatchEvent(new Event('twpxYadeliveryLocationLoad'));
+document.dispatchEvent(new Event("twpxYadeliveryLocationLoad"));
